010171f4ce9624810d52a469d0a5747c92b29ac8
andcastillo@gmail.com <andcastillo@gmail.com>
2024-07-22 16:51:17 +0800

    first commit

---
+++ tests/testthat/test-approximate-lookup.R
@@ -0,0 +1,25 @@
+test_that("Look for lactate", {
+  matches = approximate_lookup(urine_metabolites, 'synonyms_cat', 'Lactate', "lv", "lcs", limit=3)
+  expect_equal(dim(matches)[[1]], 3)
+  
+  expect_equal(matches$name[[1]], "L-Lactic acid")
+  expect_equal(matches$best_name[[1]],"Lactate")
+  expect_equal(matches$index[[1]], 97)
+  expect_equal(matches$best_score[[1]], 1)
+  
+  expect_equal(matches$name[[2]], "D-Lactic acid")
+  expect_equal(matches$best_name[[2]], "LACTate")
+  expect_equal(matches$index[[2]], 392)
+  expect_equal(matches$best_score[[2]], 1) 
+  
+  expect_equal(matches$name[[3]], "Hydroxypropionic acid")
+  expect_equal(matches$best_name[[3]], "b-Lactate")
+  
+  expect_equal(matches$index[[3]], 263)
+  expect_equal(abs(matches$best_score[[3]] - 0.7777778) < 1e-5, TRUE)
+  
+  matches = approximate_lookup(urine_metabolites, 'synonyms_cat', 'Lactate', "lv", "lcs", limit=4)
+  
+  expect_equal(dim(matches)[[1]], 4)
+  
+})
---
+++ tests/testthat/test-multiplet-search.R
@@ -0,0 +1,12 @@
+test_that("Look for lactate", {
+  query <- list(list(1.25, 1.35, 'd'), list(4.05, 4.15, 'q'))
+  result <- multiplet_query(query, hmdb_nmr_1h, urine_metabolites)
+  expected <- list("accession"=c("HMDB0000190","HMDB0000030","HMDB0000701","HMDB0002802","HMDB0001348","HMDB0014352"),
+    "name"=c("L-Lactic acid", "Biotin", "Hexanoylglycine", "Cortisone", "SM(d18:1/18:0)", "Azithromycin"),
+    "similarity"=c(2.0000000, 0.7000000, 0.4000000, 0.1428571, 0.1333333, 0.0625000))
+  expect_equal(result$accession, expected$accession)
+  expect_equal(result$name, expected$name)
+  expect_equal(abs(result$similarity[[1]] - expected$similarity[[1]]) < 1e-3, TRUE)
+  expect_equal(abs(result$similarity[[2]] - expected$similarity[[2]]) < 1e-3, TRUE)
+  expect_equal(abs(result$similarity[[3]] - expected$similarity[[3]]) < 1e-3, TRUE)
+})
